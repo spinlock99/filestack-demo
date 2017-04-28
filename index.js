24154fc7425ae69c0ae67a5e55bcfcd53abd356d
Andrew Dixon <andrew@joinhandshake.com>
2017-04-27 20:18:47 -0700

    clean up root class component

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import React, { Component } from "react";
 import { render } from "react-dom";
-import React from "react";
-import App from "./components/App";
+import { TodoList } from "./src/components/todos";
+
+class App extends Component {
+  render() {
+    const todos = [
+      { id: 0, isDone: false, text: "make components" }
+    ];
+
+    return <TodoList todos={todos} />;
+  }
+}
 
 const containerEl = document.getElementById("container");
 render(<App />, containerEl);
