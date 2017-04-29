436366c3de124b3e265ce5b5c40a3fffc2c12bd6
Andrew Dixon <andrew@joinhandshake.com>
2017-04-28 21:52:07 -0700

    add redux dev-tools

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ import AppBar from "material-ui/AppBar";
 
 class App extends Component {
   configureStore() {
-    const store = createStore(reducer);
+    const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
     if (module.hot) {
       module.hot.accept("./src/reducer", () => {
         const nextRootReducer = require("./src/reducer");
         store.replaceReducer(nextRootReducer);
       });
     }
+
     return store;
   }
+
   render() {
     const icon = "muidocs-icon-navigation-expand-more";
     return(
