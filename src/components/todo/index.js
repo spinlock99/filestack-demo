b61093d7ce947cbe196f7b27412b6fe07d0dc081
Andrew Dixon <andrew@joinhandshake.com>
2017-04-28 20:59:28 -0700

    clean up todo with array destructuring

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -2,16 +2,8 @@ import React from "react";
 import { Toggle } from "material-ui";
 
 export function Todo({ todo, handleClick }) {
-  return(
-    <Toggle
-      onClick={handleClick}
-      label={text(todo)}
-      defaultToggled={todo.get("isDone")}
-    />
-  );
-}
+  const [text, isDone] = ["text", "isDone"].map(param => todo.get(param));
+  const label = isDone ? <strike>{text}</strike> : <span>{text}</span>;
 
-function text(todo) {
-  const text = todo.get("text");
-  return todo.get("isDone") ? <strike>{text}</strike> : <span>{text}</span>;
+  return <Toggle label={label} defaultToggled={isDone} onClick={handleClick} />;
 }
