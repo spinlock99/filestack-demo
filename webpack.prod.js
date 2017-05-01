2bda0c4110905f7e4eaa883849efa2960e4c269f
Andrew Dixon <andrew@joinhandshake.com>
2017-04-30 21:07:26 -0700

    stuff

--- webpack.prod.js
+++ webpack.prod.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new ServiceWorkerWebpackPlugin({
       entry: path.join(__dirname, 'src/sw.js'),
-      publicPath: "/todo-pwa/"
+      publicPath: "/todo-pwa/sw.js"
     }),
     new CopyWebpackPlugin([{ from: "index.html" }])
   ]
